16f107e1aa4c98f93494442585d489ce8a9eb894
ApolloZhu <public-apollonian@outlook.com>
2021-04-19 03:01:40 -0400

    Write notice about the potential crash

---
+++ PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/Notice.swift
@@ -0,0 +1,1 @@
+#warning("\n⚠️ If you see this message, no worries, try re-running the page again.\n\n✅ This app has been tested on real devices and seems to work all the time; In Swift Playgrounds, there's chance that it stops running due to unknown interruptions despite nothing went wrong.\n\n⏯ Now, let me stop interrupting you, and experiment on!\n\n\n")
